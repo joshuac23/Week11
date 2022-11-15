366785ad94ab95b0a53ca4cc834b3bf9c05f9bac
Joshua <josh_jay23@hotmail.com>
2022-11-15 09:45:16 -0800

    defining functions activity

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+// activity 1: using 'function'
+
+// function greet(input) {
+//     console.log(`Hello ${input}!`)
+// }
+
+// let input = 'Joshua';
+// greet(input);
+
+
+// activity 2: defining anonymous function
+
+// let greet = function(input) {
+//     console.log(`Hello ${input}!`);
+// };
+
+// greet("Joe");
+
+
+// activity 2: defining function with arrow notation
+let greet = (input) => {
+    console.log(`Hello ${input}!`);
+}
+
+greet("Joseph");
+
+
