38d8fb2116c0df38abf19f157b8caefa05736540
Joshua <josh_jay23@hotmail.com>
2022-11-15 10:56:34 -0800

    change css styling with js

--- scripts/script.js
+++ scripts/script.js
@@ -43,11 +43,27 @@ function populate_list(list) {
     for (let i = 0 ; i<list.length ; i++) {
         
         const list_item_element = document.createElement("li"); // for each item, create an li and append to unordered_list_element
-        list_item_element.textContent = list[i]; // items still in memory, needs to be appended
-        unordered_list_element.appendChild(list_item_element); // appending to the dom element
+        list_item_element.textContent = list[i]; // items added to li elements as text. New li elements are still in memory, needs to be appended
+        unordered_list_element.appendChild(list_item_element); // appending to the dom element as a child
     }
 }
 
 populate_list(shopping_list)
+
+
+// changing CSS styling with classList
+function change_list_style(list) {
+    console.log(unordered_list_element.classList);
+    unordered_list_element.classList.add("squareList");
+    unordered_list_element.classList.remove("circleList");
+
+    // OR use: unordered_list_element.classList.replace("circleList", "squareList");
+
+}
+
+change_list_style()
+
+
+
 
 
