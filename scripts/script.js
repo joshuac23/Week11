7d4a9c099e9a477dc48fcb7a12a635ce2daa675a
Joshua <josh_jay23@hotmail.com>
2022-11-15 10:42:00 -0800

    populate shopping list (gives error

--- scripts/script.js
+++ scripts/script.js
@@ -18,10 +18,35 @@
 
 
 // activity 2: defining function with arrow notation
-let greet = (input) => {
-    console.log(`Hello ${input}!`);
+// let greet = (input) => {
+//     console.log(`Hello ${input}!`);
+// }
+
+// greet("Joseph");
+
+
+// activity 3: using default values as a parameter
+// function greet(name, time='Day') {
+//     console.log(`Hello ${name}, good ${time}!`)
+// };
+
+// greet("Dave");
+// greet("Dave", "Night");
+
+
+
+
+let shopping_list = ['cheese', 'bread', 'ham', 'red pepper'];
+const unordered_list_element = document.querySelector(".shopping"); // DOM element
+
+function populate_list(list) {
+    for (let i = 0 ; i<list.length ; i++) {
+        const list_item_element = document.createElement("li"); // for each item, create an li and append to unordered_list_element
+        list_item_element.textContent = list[i]; // items still in memory, needs to be appended
+        unordered_list_element.appendChild(list_item_element); // appending to the dom element
+    }
 }
 
-greet("Joseph");
+populate_list(shopping_list)
 
 
